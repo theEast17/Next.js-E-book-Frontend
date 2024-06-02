6021521b6330be8e7e27b086106f684492f2708f
theEast17 <nagarpoorv2001@gmail.com>
2024-06-02 16:36:45 +0530

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='border-t'>
+        <p className='text-center p-3 capitalize'>
+           &copy; 2024 Coder&#39;s Book all rights reserved
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
